b264b8583c8fc45a93d0467b07f1173c97c60ba6
Nima Hedayati <nimahedayati1380@gmail.com>
2024-03-22 11:25:53 +0330

    add font size, font weight and border radius

---
+++ lib/src/flutter_tailwind_fonts.dart
@@ -0,0 +1,41 @@
+part of flutter_tailwind_theme;
+
+class TFontSize {
+  static double xs = 12;
+  static double sm = 14;
+  static double base = 16;
+  static double lg = 18;
+  static double xl = 20;
+  static double xl2 = 24;
+  static double xl3 = 40;
+  static double xl4 = 36;
+  static double xl5 = 48;
+  static double xl6 = 60;
+  static double xl7 = 72;
+  static double xl8 = 96;
+  static double xl9 = 128;
+}
+
+class TFontWeight {
+  static FontWeight thin = FontWeight.w100;
+  static FontWeight extralight = FontWeight.w200;
+  static FontWeight light = FontWeight.w300;
+  static FontWeight normal = FontWeight.w400;
+  static FontWeight medium = FontWeight.w500;
+  static FontWeight semibold = FontWeight.w600;
+  static FontWeight bold = FontWeight.w700;
+  static FontWeight extrabold = FontWeight.w800;
+  static FontWeight black = FontWeight.w900;
+}
+
+class TFontVariationWeight {
+  static FontVariation thin = const FontVariation.weight(100);
+  static FontVariation extralight = const FontVariation.weight(200);
+  static FontVariation light = const FontVariation.weight(300);
+  static FontVariation normal = const FontVariation.weight(400);
+  static FontVariation medium = const FontVariation.weight(500);
+  static FontVariation semibold = const FontVariation.weight(600);
+  static FontVariation bold = const FontVariation.weight(700);
+  static FontVariation extrabold = const FontVariation.weight(800);
+  static FontVariation black = const FontVariation.weight(900);
+}
---
+++ lib/src/flutter_tailwind_sizes.dart
@@ -0,0 +1,13 @@
+part of flutter_tailwind_theme;
+
+class TBorderRadius {
+  static BorderRadius none = BorderRadius.zero;
+  static BorderRadius sm = BorderRadius.circular(2);
+  static BorderRadius normal = BorderRadius.circular(4);
+  static BorderRadius md = BorderRadius.circular(6);
+  static BorderRadius lg = BorderRadius.circular(8);
+  static BorderRadius xl = BorderRadius.circular(12);
+  static BorderRadius xl2 = BorderRadius.circular(16);
+  static BorderRadius xl3 = BorderRadius.circular(24);
+  static BorderRadius full = BorderRadius.circular(9999);
+}
